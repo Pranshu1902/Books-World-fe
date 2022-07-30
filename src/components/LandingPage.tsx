1622973d0e8ea38daf64b0c5472b403db5a02525
Pranshu1902 <pranshu1902@gmail.com>
2022-07-31 01:09:36 +0530

    updated landing apge

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 import logo from "../images/logo.png";
 import dashboard from "../images/dashboard.png";
 import home from "../images/home.png";
+import book from "../images/book.png";
 import { Link } from "raviger";
 import { Button } from "@material-ui/core";
 
@@ -43,15 +44,21 @@ export default function LandingPage() {
             <div className="md:w-1/2">
               <img src={dashboard} className="rounded-lg shadow" alt="icon" />
             </div>
-            <div className="text-3xl">
+            <div className="text-3xl md:w-1/2">
               Get detailed analysis on the book you read.
+            </div>
+          </div>
+          <div className="flex flex-col-reverse md:flex-row justify-center items-center gap-4">
+            <div className="text-3xl md:w-1/2">
+              Add detailed comments and track your progress.
+            </div>
+            <div className="md:w-1/2">
+              <img src={book} alt="icon" className="rounded-lg shadow" />
             </div>
           </div>
         </div>
         <div className="flex justify-center items-center pt-24">
-          <p className="text-5xl">
-            Read better so you actually understand and remember it!
-          </p>
+          <p className="text-5xl">Read better so you actually understand!</p>
         </div>
         <div className="flex flex-col justify-center items-center pt-24">
           <div className="bg-green-100 rounded-lg text-2xl p-3 shadow-lg">
